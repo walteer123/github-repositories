b45a71b7d905f3a9ef3617be970e1485a5f6402b
Walter <walter_rfl@hotmail.com>
2022-01-12 21:05:45 -0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+import version.Libs
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.walter.githubrepositories"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+                targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Libs.coreKtx)
+    implementation(Libs.appCompat)
+    implementation(Libs.material)
+    implementation(Libs.constraintLayout)
+    implementation(Libs.retrofit)
+    testImplementation(Libs.jUnit)
+    androidTestImplementation(Libs.testJUnit)
+    androidTestImplementation(Libs.espressoCore)
+}
---
+++ buildSrc/src/main/kotlin/version/Libs.kt
@@ -0,0 +1,29 @@
+package version
+
+object Libs {
+
+    private const val retrofitVersion = "2.9.0"
+    const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+
+    private const val coreKtxVersion = "1.7.0"
+    const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+
+    private const val appCompatVersion = "1.4.0"
+    const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
+
+    private const val materialVersion = "1.4.0"
+    const val material = "com.google.android.material:material:$materialVersion"
+
+    private const val constraintLayoutVersion = "2.1.2"
+    const val constraintLayout = "androidx.constraintlayout:$constraintLayoutVersion"
+
+    private const val jUnitVersion = "4.13.2"
+    const val jUnit = "junit:junit:$jUnitVersion"
+
+    private const val testJUnitVersion = "1.1.3"
+    const val testJUnit = "androidx.test.ext:junit:$testJUnitVersion"
+
+    private const val espressoCoreVersion = "3.4.0"
+    const val espressoCore = "androidx.test.espresso:espresso-core:$espressoCoreVersion"
+
+}
